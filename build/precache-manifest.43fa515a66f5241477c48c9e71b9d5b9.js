43df33e4e36b2b11ee7f56b1d2064728f6b9f26f
rania235 <rania.obeid1996@gmail.com>
2021-06-22 18:26:56 +0300

    change image links

---
+++ build/precache-manifest.43fa515a66f5241477c48c9e71b9d5b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd19558b269c5efd5d8f1da5528cfa8",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1915b79e586298fbb430",
+    "url": "/website/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2af39b1c1e9c83c88a24",
+    "url": "/website/static/css/main.dc37e0fe.chunk.css"
+  },
+  {
+    "revision": "1915b79e586298fbb430",
+    "url": "/website/static/js/2.c163f22f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/website/static/js/2.c163f22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af39b1c1e9c83c88a24",
+    "url": "/website/static/js/main.e9f75c69.chunk.js"
+  },
+  {
+    "revision": "f72e07cd09b99d83dc34",
+    "url": "/website/static/js/runtime-main.365854be.js"
+  }
+]);
