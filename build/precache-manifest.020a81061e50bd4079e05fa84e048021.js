ad73bd8e9f8086ab133f122016b7691a84c73d3e
rania235 <rania.obeid1996@gmail.com>
2021-06-27 18:28:54 +0300

    changes width

---
+++ build/precache-manifest.020a81061e50bd4079e05fa84e048021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98a5f2e33455a9a21c0935712f53f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7743b472c09b320060ed",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "12abfca5aac33ce48d22",
+    "url": "/static/css/main.347b7eda.chunk.css"
+  },
+  {
+    "revision": "7743b472c09b320060ed",
+    "url": "/static/js/2.93b5eae4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.93b5eae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12abfca5aac33ce48d22",
+    "url": "/static/js/main.1aec8916.chunk.js"
+  },
+  {
+    "revision": "9d0cc0742a935355addc",
+    "url": "/static/js/runtime-main.523afb3e.js"
+  }
+]);
