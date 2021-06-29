6931c3be4e19bcb72d0312265d074c4183c99752
rania235 <rania.obeid1996@gmail.com>
2021-06-29 11:06:19 +0300

    Updates

--- precache-manifest.4a2f4a6ff6c028882ef7e858d633337b.js
+++ precache-manifest.4a2f4a6ff6c028882ef7e858d633337b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c0b592f96e652b6fcdedb122a71c84b",
+    "revision": "c799a74726a0831e8c0471f943dd0554",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "c9b7f8bbc097b513533d",
-    "url": "/static/css/main.eb458e06.chunk.css"
+    "revision": "49d6119ad67b118e1e39",
+    "url": "/static/css/main.b7421ac1.chunk.css"
   },
   {
     "revision": "67fc75b574eea5b8565c",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d2ee6e4e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c9b7f8bbc097b513533d",
-    "url": "/static/js/main.e9fdf38c.chunk.js"
+    "revision": "49d6119ad67b118e1e39",
+    "url": "/static/js/main.2755ba6b.chunk.js"
   },
   {
     "revision": "9d0cc0742a935355addc",
