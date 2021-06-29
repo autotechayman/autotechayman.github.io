1ca94def36fd4262d6c7ea34b9e9ec0bba325093
Bassel Kanso <basselkanso82@gmail.com>
2021-06-29 15:37:50 +0300

    fixing 3 dots

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74d0cb74fbbdf1355e1a99f9351790f8.js"
+  "/precache-manifest.5dc1002bd713252350b4616f59a9611c.js"
 );
 
 self.addEventListener('message', (event) => {
