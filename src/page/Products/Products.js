4e86cf636551e0962173f0fe76ef363bffd49d02
rania235 <rania.obeid1996@gmail.com>
2021-06-29 08:11:25 +0300

    change gallery

--- src/page/Products/Products.js
+++ src/page/Products/Products.js
@@ -7,111 +7,79 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class Products extends React.Component {
  render(){
    return(
+     <>
+
+      <div>
+            <p className="our-work">OUR WORK</p>
+      </div>
+
+
      <div className="Bakhour--products">
-       <p>our Work</p>
-{/* <div className="wrapper">
-     <header>
-<div className="wrapper-text-box">
-<p className="title-header-bakhour">Our Work and designs before and after </p>
-</div>
-    </header>
+      
+       <div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
-<div className>
-<div className="row">
-<div className="column">
-<img  src='/Image/car4.jpg' id="row-pic"/>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
-
-
-
-<div className="column">
-<img  src='/Image/car5.webp' id="row-pic"column/>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
+</div>
 
 
-<div className="column">
-<img  src='/Image/car6.jpg' id="row-pic"/>
-
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
 
-<div className="column">
-<img  src='/Image/car7.jpg' id="row-pic"/>
-</div>
-</div>
-
-
-<center><div className="wrapper">
-<header >
-<div className="wrapper-text-box">
-<p className="title-header-bakhour-skincare">Our skincare essentials are designed to make you look and feel your glowy, dewy best before you even think about makeup.</p>
-</div>
-</header>
-</div>
-</center>
-
-
-
-<div className="row">
-<div className="column">
-<img  src='/Image/range1.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">Before</p1>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
 
-<div className="column">
-<img  src='/Image/range2.jpg' id="row-pic"/><p1 className="title-header-bakhour1">After</p1>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
 
-
-<div className="column">
-<img  src='/Image/range5.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">Before</p1>
-</div>
-
-
-<div className="column">
-<img  src='/Image/range6.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">After</p1>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
 
-<div className="column">
-<img  src='/Image/range7.jpg' id="row-pic"/><p1 className="title-header-bakhour1">Before</p1>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
-
-<div class="column">
-<img  src='/Image/range8.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">After</p1>
+<div class="card">
+  <img src='/Image/car1.jpg' alt="John" />
+  <h1 className="john-bakhour">John Doe</h1>
 </div>
 
 
-<div class="column">
-<img  src='/Image/range11.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">Before</p1>
-
 </div>
-
-
-<div class="column">
-<img  src='/Image/range12.jpg' id="row-pic"/>
-<p1 className="title-header-bakhour1">After</p1>
-
-</div>
-</div>
-</div> */}
-</div>
+</>
 
 
 
