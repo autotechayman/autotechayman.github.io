83c013e4eed012cd87870a9b0bcecc79afc222e6
rania235 <rania.obeid1996@gmail.com>
2021-06-30 13:45:52 +0300

    last change1

--- src/page/Event/Event.js
+++ src/page/Event/Event.js
@@ -10,7 +10,7 @@ class Events extends Component {
     render() {
         return (
           <div className="contact-us-bakhour">
-              <form className="form-bakhour" action="https://formspree.io/f/xpzklraw" method="POST">
+              <form className="form-bakhour"  method="POST" action="https://formspree.io/f/xpzklraw">
   <div>
   <img src="https://cdn4.iconfinder.com/data/icons/basic-user-interface-elements/700/mail-letter-offer-256.png" alt="icon"/>
    </div>
@@ -19,7 +19,7 @@ class Events extends Component {
   <input type="text" placeholder="Email:" required/>
   <input type="text" placeholder="phone number:" required/>
   <textarea placeholder="Messages:" required></textarea>
-  <input type="submit" value="Send"/>
+  <input type="submit" value="Send" />
 </form>
 
             </div>
